b3f5aaf6b69eddde761d237ca7c73f91e6003343
bryant_wang <bryant_wang@paradise-soft.com.tw>
2021-12-29 02:00:09 +0800

    feat: change const to var

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const linebot = require('linebot');
-const express = require('express')
-const app = express()
-const port = 
+var linebot = require('linebot');
+var express = require('express')
+var app = express()
+var port = 80
 
-const bot = linebot({
+var bot = linebot({
   channelId: '1656718211',
   channelSecret: '65e2264b18c161d37bdb324c288c4372',
   channelAccessToken: 'TNIYI9izHfTZur7p1sWeSrG5ra0c96hoaP8EZ0L+5VDmS3sg7iuPagaoQSZsd4rJQLUycIHhYS3Gcisv9UiTW6USiuDZ5EeSl7AWvgICzW1b9TQLF6Vq3yCr6DwT1Xp1kwyLQWcHeSoVJmgYd0/GtwdB04t89/1O/w1cDnyilFU='
